a9ec26c2f7fbd857043b406a8b73ba5bb27942e8
Ervin Flores Sucapuca <efloressuc@unsa.edu.pe>
2023-07-04 00:26:06 -0500

    Avance de laboratorio

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "home.html", {'name':'IRVIN'})
+
+def add(request):
+    
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2 
+
+    return render(request, 'result.html', {'result':res})
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('calc.urls')),
+    path('', include('viajes.urls')),
     path('admin/', admin.site.urls),
 
 ]
